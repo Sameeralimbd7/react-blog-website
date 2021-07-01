6afa180e68118936cd718ea21111a38d009396df
Sameer Ali <77734212+sameeralimbd7@users.noreply.github.com>
2021-07-01 15:15:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
   static: "./build",
 });
 server.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
