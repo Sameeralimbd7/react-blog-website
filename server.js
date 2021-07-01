66a9d7eb8b7e907412b9aca9bfbc0b11bff1dd41
Sameer Ali <77734212+sameeralimbd7@users.noreply.github.com>
2021-07-01 15:26:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const server = require('express');
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
+
+server.use(middlewares);
+server.use("/api", jsonServer.router("db.json"));
 server.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
-  })
-})
-server.use(middlewares);
-server.use("/api", jsonServer.router("db.json"));
+  });
+});
