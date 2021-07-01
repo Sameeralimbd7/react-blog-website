db254c6763e724e6f0a172499952bb3c8db14799
Sameer Ali <77734212+sameeralimbd7@users.noreply.github.com>
2021-07-01 21:10:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const path = require('path');
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+server.use(jsonServer.defaults());
 server.use(router);
-
+server.use(server.static(path.join(__dirname, 'build')));
+server.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 server.listen(port);
