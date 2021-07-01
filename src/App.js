4a21265e5660ae1c25aa499af565acde9eb04275
Sameer Ali <77734212+sameeralimbd7@users.noreply.github.com>
2021-07-01 14:40:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Create from "./components/create/Create";
 import BlogDes from "./components/BlogDes";
 import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -27,9 +27,7 @@ function App() {
                     <Route path="/blogs/:id">
                       <BlogDes />
                     </Route>
-                    <Route exact path="*">
-                      <Home />
-                    </Route>
+                    <Redirect to='/' />
                   </Switch>
                 </ScrollToTop>
               </main>
