76ba71d1896b927cf3cbaf4f154214ed24b1ef75
Sameer Ali <77734212+sameeralimbd7@users.noreply.github.com>
2021-07-02 11:21:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const path = require('path')
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
 const port = process.env.PORT || 8000;
 server.use(middlewares);
+app.use(jsonServer.router(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 server.use('/api', router);
 server.listen(port);
